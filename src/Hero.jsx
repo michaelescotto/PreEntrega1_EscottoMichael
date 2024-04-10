46586ebe4b55d20194249783985b2f689591a46a
Michael Escotto <michadacu@gmail.com>
2024-04-09 23:10:26 -0300

    Configurando el espacio

---
+++ src/Hero.jsx
@@ -0,0 +1,10 @@
+import HeroImage from './assets/images/hero.png'
+const Hero = () => {
+    return (
+        <> 
+            <img src={HeroImage} alt="Hero" />
+        </>
+    )
+}
+
+export default Hero
